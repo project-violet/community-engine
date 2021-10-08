c63f9fd4cd135611e1fd9476cb71cac8223257f8
violet-dev <violet.dev.master@gmail.com>
2021-10-08 03:25:43 +0000

    test(entry): change test format

--- server/tests/post/test_post.js
+++ server/tests/post/test_post.js
@@ -3,16 +3,6 @@
 
 const t_a_post = require("./test_post_article");
 
-async function test_post() {
-  var modules = {
-    "test-post-article": t_a_post,
-  };
-
-  for (var key in modules) {
-    var result = await modules[key]();
-    if (result) console.log(key, ":", "\x1b[32mSuccess\x1b[0m");
-    else console.log(key, ":", "\x1b[31mFail\x1b[0m");
-  }
-}
-
-module.exports = test_post;
+module.exports = {
+  "test-post-article": t_a_post,
+};
--- server/tests/test_entry.js
+++ server/tests/test_entry.js
@@ -3,4 +3,28 @@
 
 const t_post = require("./post/test_post");
 
-t_post().then();
+var modules = [];
+
+modules.push(t_post);
+
+async function run_test() {
+  for (var module of modules) {
+    for (var key in module) {
+      try {
+        var result = await module[key]();
+
+        console.log(
+          "(not throw)",
+          key,
+          ":",
+          result ? "\x1b[32mSuccess\x1b[0m" : "\x1b[31mFail\x1b[0m"
+        );
+      } catch (e) {
+        console.log("(throw)", key, ":", "\x1b[31mFail\x1b[0m");
+        console.log(e);
+      }
+    }
+  }
+}
+
+run_test().then();
