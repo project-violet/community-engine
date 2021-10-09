685eb5376f20c1b1bc0fea5ca19a488491335f01
violet-dev <violet.dev.master@gmail.com>
2021-10-09 09:20:10 +0000

    impl(logger): implements logger

---
+++ server/etc/logger.js
@@ -0,0 +1,23 @@
+// This source code is a part of Project Violet.
+// Copyright (C) 2021. violet-team. Licensed under the Apache-2.0 License.
+
+const { createLogger, format, transports } = require("winston");
+const { combine, timestamp, printf, json, splat, prettyPrint } = format;
+
+require("winston-daily-rotate-file");
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} - ${level}: ${JSON.stringify(message, null, 2)}`;
+});
+
+var transport = new transports.DailyRotateFile({
+  filename: "./log/%DATE%.log",
+  datePattern: "YYYY-MM-DD",
+});
+
+var logger = createLogger({
+  format: combine(timestamp(), json(), splat(), prettyPrint(), myFormat),
+  transports: [transport],
+});
+
+module.exports = logger;
